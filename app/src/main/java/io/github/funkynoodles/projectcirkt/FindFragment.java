6cf82464bd5dbc8e81c303d88223150615fd4563
Funky Noodles <nickasds@hotmail.com>
2015-09-14 13:10:42 -0500

    Added color.xml resources

--- app/src/main/java/io/github/funkynoodles/projectcirkt/FindFragment.java
+++ app/src/main/java/io/github/funkynoodles/projectcirkt/FindFragment.java
@@ -23,17 +23,12 @@ public class FindFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-            /*Spinner spinner = (Spinner) getView().findViewById(R.id.timeSpinner);
-            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
-                    R.array.timeArray, android.R.layout.simple_spinner_item);
-            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-            spinner.setAdapter(adapter);*/
         View myFragmentView = inflater.inflate(R.layout.fragment_find, container, false);
-        Spinner dropdown = (Spinner)myFragmentView.findViewById(R.id.timeSpinner);
-        String[] items = new String[]{"Saturn V", "2", "three"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_spinner_item, items);
-        dropdown.setAdapter(adapter);
+        Spinner spinner = (Spinner) myFragmentView.findViewById(R.id.timeSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.timeArray, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
         return myFragmentView;
 
     }
